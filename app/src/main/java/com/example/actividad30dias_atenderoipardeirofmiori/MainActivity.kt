f116aad98604a36ad5f8578f1da9f344a61c7b9d
adrecife <atendero25@gmail.com>
2025-01-15 11:55:59 +0100

    Test1UnoPruebaIncial

--- app/src/main/java/com/example/actividad30dias_atenderoipardeirofmiori/MainActivity.kt
+++ app/src/main/java/com/example/actividad30dias_atenderoipardeirofmiori/MainActivity.kt
@@ -33,7 +33,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "No olvidaré Road Atlanta",
+        text = "No olvidaré Road Atlana",
         modifier = modifier
     )
 }
