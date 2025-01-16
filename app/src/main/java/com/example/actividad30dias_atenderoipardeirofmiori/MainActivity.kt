ba497975b017a2b534af4e3da05349f13f2c6ee8
Federico Miori <federicomimart@gmail.com>
2025-01-16 13:51:25 +0100

    Ramas unidas

--- app/src/main/java/com/example/actividad30dias_atenderoipardeirofmiori/MainActivity.kt
+++ app/src/main/java/com/example/actividad30dias_atenderoipardeirofmiori/MainActivity.kt
@@ -6,11 +6,13 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.actividad30dias_atenderoipardeirofmiori.model.Circuito
 import com.example.actividad30dias_atenderoipardeirofmiori.ui.theme.Actividad30Dias_ATenderoIPardeiroFMioriTheme
 
 class MainActivity : ComponentActivity() {
@@ -26,6 +28,25 @@ class MainActivity : ComponentActivity() {
                     )
                 }
             }
+        }
+    }
+}
+
+@Composable
+fun CircuitoApp() {
+    CircuitoList(
+        circuitoList = Datasource().loadCircuito,
+    )
+}
+
+@Composable
+fun CircuitoList(circuitoList: List<Circuito>, modifier: Modifier = Modifier) {
+    LazyColumn(modifier = modifier) {
+        items(circuitoList) { circuito ->
+            circuitoCard(
+                circuito = circuito,
+                modifier = Modifier.padding(8.dp)
+            )
         }
     }
 }
